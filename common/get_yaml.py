eac08d6922362c73b04b93ac4ddb9216e046bc96
ysh925 <65474405+ysh925@users.noreply.github.com>
2020-06-05 07:16:29 +0800

    python

--- common/get_yaml.py
+++ common/get_yaml.py
@@ -1,7 +1,7 @@
 import yaml,os
 
 def get_yaml(file_name):
-    file_path = os.path.abspath('../') + os.sep + "Data" + os.sep + file_name + ".yaml"
+    file_path = "D:\Program Files (x86)\python\demo\Data" + os.sep + file_name + ".yaml"
 
     with open(file_path,'r',encoding='utf-8') as f:
         return yaml.load(f,Loader=yaml.FullLoader)
